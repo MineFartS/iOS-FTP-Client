0907a6e46b1eb4189b80ccf20d41ed209fe2c6d9
Phil Hunt <philiphuntiv2@gmail.com>
2026-04-07 13:05:54 -0400

    Initial Commit

---
+++ FTPClient/FTPClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  FTPClientApp.swift
+//  FTPClient
+//
+//  Created by Virtual Machine User on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct FTPClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
