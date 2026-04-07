bf4c3d72ab9bb687352cdbed125ae027aee614a1
Phil Hunt <philiphuntiv2@gmail.com>
2026-04-07 13:55:20 -0400

    Update ContentView.swift

--- FTPClient/ContentView.swift
+++ FTPClient/ContentView.swift
@@ -6,19 +6,8 @@
 //
 
 import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+import LoginForm
 
 #Preview {
-    ContentView()
+    LoginForm()
 }
